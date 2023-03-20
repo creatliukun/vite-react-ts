697b0a5ceb8ba1ce09d90309520fe26d8c93715d
creatliukun <952476356@qq.com>
2023-03-20 22:20:02 +0800

    feat: 搭建基础路由

--- src/App.tsx
+++ src/App.tsx
@@ -5,21 +5,10 @@ import { Button } from 'antd'
 // import 'antd/dist/antd.css' // or 'antd/dist/antd.less' // 全局引入
 import { useRoutes, Link } from 'react-router-dom'
 import router from '@/router'
-// import About from './views/About'
 
 function App() {
-  const [count, setCount] = useState(0)
   const outlet = useRoutes(router)
-  return (
-    <div className="App">
-      <div>分割1</div>
-      <Link to="/about">about</Link>
-      <div>分割2</div>
-      <Link to="/home">home</Link>
-      {outlet}
-      {/* <About /> */}
-    </div>
-  )
+  return <div className="App">{outlet}</div>
 }
 
 export default App
--- src/router/index.tsx
+++ src/router/index.tsx
@@ -1,27 +1,46 @@
-import Home from '@/views/Home'
-import About from '@/views/About'
-import Login from '@/views/Login'
+import React, { lazy } from 'react'
+import { Navigate } from 'react-router-dom'
+
+// 封装路由懒加载
+const Login = lazy(() => import('@/views/Login'))
+const About = lazy(() => import('@/views/About'))
+const Home = lazy(() => import('@/views/Home'))
+const Page1 = lazy(() => import('@/views/pages/page1'))
+const Page2 = lazy(() => import('@/views/pages/page2'))
 
 // Navigate重定向组件
-import { Navigate } from 'react-router-dom'
-// 路由表写法
+const withLoadingComponent = (comp: JSX.Element) => (
+  <React.Suspense fallback={<div>loading...</div>}>{comp}</React.Suspense>
+)
 
 const routes = [
+  // 嵌套路由，开始-----
   {
     path: '/',
-    element: <Navigate to="about" />,
+    element: <Navigate to="home" />,
   },
   {
-    path: '/login',
-    element: <Login />,
+    path: '/',
+    element: <Home />,
+    children: [
+      {
+        path: '/page1',
+        element: withLoadingComponent(<Page1 />),
+      },
+      {
+        path: '/Page2',
+        element: withLoadingComponent(<Page2 />),
+      },
+      {
+        path: '/login',
+        element: withLoadingComponent(<Login />),
+      },
+      {
+        path: '/about',
+        element: withLoadingComponent(<About />),
+      },
+    ],
   },
-  {
-    path: '/about',
-    element: <About />,
-  },
-  {
-    path: '/home',
-    element: <Home num={undefined} setNum={undefined} />,
-  },
+  // 嵌套路由，结束-----
 ]
 export default routes
